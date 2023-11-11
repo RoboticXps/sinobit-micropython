a4f623f30224dcceb3b9ffca96bdd063931e8060
MeqdadDev <meqdad.darweesh@gmail.com>
2023-11-11 14:25:58 +0200

    upload counter example

---
+++ counter.py
@@ -0,0 +1,16 @@
+"""
+    counter.py
+    ~~~~~~~~~~
+    Creates a counter that increments on pressing button a.
+    Decrements on pressing button b.
+    Scrolls the current count on the led display.
+"""
+
+import microbit
+ctr = 0                                         # Initialize counter
+while True:                                     # Loop forever
+    ctr = ctr + microbit.button_a.get_presses() # Count the amount of presses
+    ctr = ctr - microbit.button_b.get_presses() # Count the amount of presses 
+    if ctr > 0:                                 # Only execute if not zero
+        microbit.display.scroll(str(ctr))       # Display the counter
+    microbit.sleep(100)                         # Sleep for 100ms
